095809e1d1ff2322a6f328db5f5041551d39b576
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-19 13:16:54 -0600

    🔧 chore: Add Tailwindcss and configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/components/**/*.jsx',
+    './src/pages/**/*.jsx',
+    './src/**/*.jsx',
+    './src/index.html'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};

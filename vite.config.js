01fe27d13efc6e239133f20398520d2f4ffea202
Fabrizio <fabgrel10@gmail.com>
2022-03-21 23:06:47 -0600

    ✏️ fix: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr;
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({

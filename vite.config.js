ca9a83be81fee019f9305903c601d54f6b4b2daa
Fabrizio <fabgrel10@gmail.com>
2022-08-29 13:31:04 -0500

    ⬆️ Upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist'
-  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
       babel: {
         plugins: ['@emotion/babel-plugin']
-      }
-    }),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true
       }
     })
   ]
